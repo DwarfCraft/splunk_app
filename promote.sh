fb7358c20e01baa152be3a628ec174c1f94663c6
root <root@localhost.localdomain>
2020-03-05 13:47:37 -0500

    perm hange

--- promote.sh
+++ promote.sh
@@ -6,4 +6,6 @@ TARGET_DIR="/opt/splunk/etc/deployment-apps"
 cp -R ${SOURCE_DIR}/* ${TARGET_DIR}
 rm "${TARGET_DIR}/README.md"
 rm "${TARGET_DIR}/promote.sh"
+chown -R splunk.splunk "${TARGET_DIR}/*"
+
 
