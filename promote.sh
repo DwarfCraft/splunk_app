6171582ee53fa1ea3db137fda1d83296b760cdaa
root <root@localhost.localdomain>
2020-03-05 13:22:26 -0500

    deploy script

---
+++ promote.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+SOURCE_DIR=`dirname $(readlink -f "$0")`
+TARGET_DIR="/opt/splunk/etc/deployment-apps"
+
+cp -R ${SOURCE_DIR}/* ${TARGET_DIR}
+rm "${TARGET_DIR}/README.md"
+rm "${TARGET_DIR}/promote.sh"
+
